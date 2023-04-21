50f609b39136c62680bcabf79b935de7de5ae2b3
敬志强 <437936609@qq.com>
2023-04-21 14:14:32 +0800

    1

--- cms/src/common/config.js
+++ cms/src/common/config.js
@@ -1,5 +1,7 @@
- const Api_url='/../' 
-//const Api_url='http://www.zzz.cn/' 
+ const Api_url = 'https://api.aku.pub/'
+ //const Api_url='http://www.zzz.cn/' 
 
 
-export {Api_url}
+ export {
+ 	Api_url
+ }
