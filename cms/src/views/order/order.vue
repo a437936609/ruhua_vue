2eb7e6084f46d9da9d05e32025f7a19d01ec8a7c
lcbpeixg <5502758@qq.com>
2023-05-24 17:59:51 +0800

    订单列表优化

--- cms/src/views/order/order.vue
+++ cms/src/views/order/order.vue
@@ -141,7 +141,6 @@
 						<!-- 订单列表 -->
 						<div v-if="!addShow" style="padding: 15px;background-color: #fff">
 							<el-table :data="list" border style="width: 100%" @filter-change="xxx">
-								<el-table-column type="index" label="序号" width="50"></el-table-column>
 								<el-table-column label="订单号" width="170">
 									<template slot-scope="scope">
 										<del v-if="scope.row.prepay_id == null" style="color: #d0d0d0;">未完成付款</del>
@@ -151,8 +150,9 @@
 								<el-table-column label="商品名称" prop="goods_id" width="280" :filters="goods_list"
 									:filter-method="filterHandler">
 									<template slot-scope="scope">
-										<div v-for="(item, key) of scope.row.ordergoods" :key="key">
+										<div v-for="(item, key) of scope.row.ordergoods" :key="key" style="border-bottom:1px dotted #CCC;padding-bottom:10px">
 											{{item.goods_name | ellipsis}}
+											<div style="font-size:10px;color:#999">{{item.sku_name}} × {{item.num}}</div>
 										</div>
 									</template>
 								</el-table-column>
@@ -182,7 +182,7 @@
 										</div>
 									</template>
 								</el-table-column>
-								<el-table-column label="姓名/手机号" width="120">
+								<el-table-column label="姓名/手机号/地址">
 									<template slot-scope="scope">
 										<div>{{scope.row.receiver_name}}</div>
 										<div>{{scope.row.receiver_mobile}}</div>
@@ -1277,7 +1277,12 @@
 	.order {
 
 		margin-top: 0;
-
+		
+		.order-spec-item:not(:last-child) {
+			border-bottom: 1rpx solid #EEEEEE;
+		}
+		
+		
 		.el-collapse-item__content {
 			padding-bottom: 0px;
 		}
